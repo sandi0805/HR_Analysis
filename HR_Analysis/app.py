7c2e8f5ccd0c7d1a04e5d04bf759a6ef53656991
Sandra Hall <71942530+sandi0805@users.noreply.github.com>
2021-02-04 22:48:11 -0500

    Add files via upload
    Update from 2/4 Session

--- HR_Analysis/app.py
+++ HR_Analysis/app.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import json
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+import pymysql
+from config import remote_db_endpoint, remote_db_port, remote_db_name, remote_db_user, remote_db_pwd
+
+# set up MySQL and connect
+pymysql.install_as_MySQLdb()
+engine = create_engine(f"mysql://{remote_db_user}:{remote_db_pwd}@{remote_db_endpoint}:{remote_db_port}/{remote_db_name}")
 
 app = Flask(__name__)
 
@@ -17,17 +24,22 @@ def index():
 
 @app.route("/EmployeeData")
 def Employee_Data():
+
+    conn = engine.connect()
+
     # Opening csv data file 
-    f = pd.read_csv('Datasets/employee_attrition_train.csv')
-    json_f = f.to_json(orient='records')
-    parsed = json.loads(json_f)
-    json_h = json.dumps(parsed,indent = 4)
+    employee_survey_df = pd.read_sql('SELECT * FROM employee_survey', con=conn)
+    employee_survey_json = employee_survey_df.to_json(orient='records')
+    #parsed = json.loads(json_f)
+    #json_h = json.dumps(parsed,indent = 4)
     #Employee_Data = csv.load(f)
 
     # open the csv data file
     # return that csv through the endpoint we created
   
-    return json_h
+    return employee_survey_json
+
+    conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
