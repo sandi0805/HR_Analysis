cd13bac269faba8cd418f065cdd98983fc4223aa
sandi0805 <sandee006xx@hotmail.com>
2021-02-11 19:53:34 -0500

    Gauge Chart and app.py update

--- app.py
+++ app.py
@@ -133,6 +133,40 @@ def educ():
   #Return json to client
     return data_json
 
+# dept_gender_stats endpoint
+@app.route("/api/dept_gender_stats")
+def dept_gender_stats():
+
+    conn = engine.connect()
+
+    query = '''
+        SELECT 
+            gender,
+            department,
+            AVG(MonthlyIncome * 12) AS annual_income_avg,
+            SUM(CASE Attrition WHEN 'YES' THEN 1 ELSE 0 END) AS attrition_sum,    
+            COUNT(*) AS employee_count,
+            AVG(JobSatisfaction) AS job_satisfaction_avg,
+            AVG(YearsAtCompany) AS tenure_avg
+        FROM 
+            employee_survey
+        GROUP BY
+            gender,
+            department
+        ORDER BY
+            department
+    '''
+
+    # Opening csv data file 
+    data_df = pd.read_sql(query, con=conn)
+    data_json = data_df.to_json(orient='records')
+   
+    # Close database connection
+    conn.close()
+
+    #Return json to client
+    return data_json
+
 @app.route("/api/Annualcostturnover")
 def employeecount():
 
@@ -157,10 +191,79 @@ def employeecount():
   #Return json to client
     return annual_cost_turnover_Json
 
-
-
 #close db connection
+    
+# Average Job Satisfaction
+
+@app.route("/api/job_satisfaction_avg")
+def job_satisfaction_avg():
+
+   conn = engine.connect()
+
+   job_satisfaction_avg = pd.read_sql('SELECT AVG(JobSatisfaction) AS job_satisfaction_avg FROM employee_survey', con=conn)
+   job_satisfaction_avg_json = job_satisfaction_avg.to_json(orient='records')
+ 
+
+   conn.close()
+
+   return job_satisfaction_avg_json
+
+
+@app.route("/api/genderdemogrpahic")
+def genderdemogrpahic():
+
+    conn = engine.connect()
+  
+    query = '''
+     SELECT 
+	    (MonthlyIncome * 12) AS annual_income,
+        CASE Attrition WHEN 'YES' then 1 ELSE 0 END AS attrition,    
+        Gender AS gender,
+        Department AS department,
+        JobSatisfaction AS jobsatisfaction,
+        YearsAtCompany AS yearsatcompany
+    FROM 
+	    employee_survey 
+	WHERE  NOT (attrition is NULL OR
+		gender IS NULL OR
+        department IS NULL OR
+        jobsatisfaction IS NULL OR
+        yearsatcompany IS NULL);
+    '''
+    Gender_Demographic_df = pd.read_sql(query, con=conn)
+    Gender_Demographic_json = Gender_Demographic_df.to_json(orient='records')
+
     conn.close()
+    return Gender_Demographic_json
+
+#close DB connection
+
+@app.route("/api/genderIncome")
+def genderIncome(): 
+
+    conn = engine.connect()
+
+    query = '''
+
+    SELECT 
+        Department,
+        AVg(MonthlyIncome) AS Income,
+        Gender
+    FROM 
+        employee_survey
+    WHERE Attrition = "Yes"
+    Group by MonthlyIncome
+    ORDER BY Department
+
+    '''
+
+
+    gender_income_df = pd.read_sql(query, con=conn)
+    gender_income_json = gender_income_df.to_json(orient='records')
+
+    conn.close()
+    return gender_income_json
+
 
 #run the app in debug mode
 if __name__ == "__main__":
--- static/js/app.js
+++ static/js/app.js
@@ -31,11 +31,16 @@ d3.json('/api/avg_rating_by_dept').then(data => {
  * GAUGE CHART
  */
 
+d3.json('/api/job_satisfaction_avg').then(function(data) {
+  var happiness = data.map(d => d['job_satisfaction_avg']);
+
+console.log(happiness);
+
 var data = [
 	{
-		domain: { x: [0, 1], y: [0, 1] },
-		value: 270,
-		title: { text: "Speed" },
+		domain: { x: [0,1], y: [0,1] },
+		value: 2.7123,
+		title: { text: "Average Job Satisfaction" },
 		type: "indicator",
 		mode: "gauge+number"
 	}
@@ -43,6 +48,8 @@ var data = [
 
 var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
 Plotly.newPlot('intro-gauge-chart', data, layout);
+
+});
 
 /*******
  * LINE GRAPH
